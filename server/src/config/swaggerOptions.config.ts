849838695bbd29779c08a8beb6d7fa0513c21e23
Juudini <juudinidev@gmail.com>
2023-10-04 16:52:40 -0300

    refactor(config/swaggerOptions): Improved tags

--- server/src/config/swaggerOptions.config.ts
+++ server/src/config/swaggerOptions.config.ts
@@ -7,6 +7,24 @@ export const swaggerOptions = {
             description: "A simple express library API",
         },
         servers: [{ url: "http://localhost:3000/api/v1" }],
+        tags: [
+            {
+                name: "Pizzas",
+                description: "Operations about pizzas",
+            },
+            {
+                name: "Empanadas",
+                description: "Operations about empanadas",
+            },
+            {
+                name: "Beverages",
+                description: "Operations about beverages",
+            },
+            {
+                name: "Desserts",
+                description: "Operations about desserts",
+            },
+        ],
     },
     apis: ["./src/routes/*/*.ts"],
 };
