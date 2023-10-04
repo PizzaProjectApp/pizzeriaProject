799bd7da2d1c151eea41ff10fbad46764e0adb86
Juudini <juudinidev@gmail.com>
2023-10-04 16:42:59 -0300

    refactor(config/swaggerOptions): Improved version and apis path

--- server/src/config/swaggerOptions.config.ts
+++ server/src/config/swaggerOptions.config.ts
@@ -1,12 +1,12 @@
 export const swaggerOptions = {
     definition: {
-        openapi: "3.0.0",
+        openapi: "3.0.3",
         info: {
             title: "Pizzeria E-commerce API",
             version: "1.0.0",
             description: "A simple express library API",
         },
-        servers: [{ url: "http://localhost:3000" }],
+        servers: [{ url: "http://localhost:3000/api/v1" }],
     },
-    apis: ["./src/routes/*.ts"],
+    apis: ["./src/routes/*/*.ts"],
 };
