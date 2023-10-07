eca94294214e97480be2392b728d7ae2fdaa14b9
Juudini <juudinidev@gmail.com>
2023-10-07 16:23:47 -0300

    refactor(routes/v1): Moved Swagger Docs to their new folder

--- server/src/routes/v1/empanada.routes.ts
+++ server/src/routes/v1/empanada.routes.ts
@@ -6,149 +6,17 @@ const empanadaController = new EmpanadaController();
 
 export default class EmpanadaRouter extends CustomRouter {
     init() {
-        /**
-         * @swagger
-         * components:
-         *   schemas:
-         *     Empanada:
-         *       type: object
-         *       properties:
-         *         name:
-         *           type: string
-         *         description:
-         *           type: string
-         *         price:
-         *           type: number
-         *         thumbnail:
-         *           type: array
-         *           items:
-         *             type: string
-         *           default: [""]
-         *           description: Array of thumbnail URLs (["www.imagen1.com", "www.imagen2.com"])
-         */
-
-        /**
-         * @swagger
-         * tags:
-         *   - name: Empanadas
-         *     description: Operations related to empanadas
-         */
-
-        /**
-         * @swagger
-         * /empanadas:
-         *   get:
-         *     summary: Get a list of available empanadas
-         *     tags:
-         *       - Empanadas
-         *     responses:
-         *       200:
-         *         description: List of empanadas retrieved successfully
-         *       500:
-         *         description: Internal server error
-         */
-
         //~> |Get a list of available empanadas
         this.get("/", empanadaController.getEmpanadas);
 
-        /**
-         * @swagger
-         * /empanadas:
-         *   post:
-         *     summary: Add a new empanada
-         *     tags:
-         *       - Empanadas
-         *     requestBody:
-         *       required: true
-         *       content:
-         *         application/json:
-         *           schema:
-         *             $ref: '#/components/schemas/Empanada'
-         *     responses:
-         *       201:
-         *         description: Empanada added successfully
-         *       400:
-         *         description: Bad request
-         */
         //~> |Add a new empanada
         this.post("/", empanadaController.addEmpanada);
-
-        /**
-         * @swagger
-         * /empanadas/{empid}:
-         *   get:
-         *     summary: Get an empanada by ID
-         *     tags:
-         *       - Empanadas
-         *     parameters:
-         *       - name: empid
-         *         in: path
-         *         required: true
-         *         description: ID of the empanada to get
-         *         schema:
-         *           type: string
-         *     responses:
-         *       200:
-         *         description: Empanada retrieved successfully
-         *       404:
-         *         description: Empanada not found
-         */
 
         //~> |Get an empanada by ID
         this.get("/:empid", empanadaController.getEmpanadaById);
 
-        /**
-         * @swagger
-         * /empanadas/{empname}:
-         *   put:
-         *     summary: Update an empanada by name
-         *     tags:
-         *       - Empanadas
-         *     parameters:
-         *       - name: empname
-         *         in: path
-         *         required: true
-         *         description: Name of the empanada to update
-         *         schema:
-         *           type: string
-         *     requestBody:
-         *       required: true
-         *       content:
-         *         application/json:
-         *           schema:
-         *             $ref: '#/components/schemas/Empanada'
-         *     responses:
-         *       200:
-         *         description: Empanada updated successfully
-         *       404:
-         *         description: Empanada not found
-         *       400:
-         *         description: Bad request
-         */
-
         //~> |Update an empanada by name
         this.put("/:empname", empanadaController.updateEmpanadas);
-
-        /**
-         * @swagger
-         * /empanadas/{empname}:
-         *   delete:
-         *     summary: Delete an empanada by name
-         *     tags:
-         *       - Empanadas
-         *     parameters:
-         *       - name: empname
-         *         in: path
-         *         required: true
-         *         description: Name of the empanada to delete
-         *         schema:
-         *           type: string
-         *     responses:
-         *       204:
-         *         description: Empanada deleted successfully
-         *       404:
-         *         description: Empanada not found
-         */
 
         //~> |Delete an empanada by name
         this.delete("/:empname", empanadaController.deleteEmpanadas);
--- server/src/routes/v1/pizza.routes.ts
+++ server/src/routes/v1/pizza.routes.ts
@@ -6,153 +6,17 @@ const pizzaController = new PizzaController();
 
 export default class PizzaRouter extends CustomRouter {
     init() {
-        /**
-         * @swagger
-         * components:
-         *   schemas:
-         *     Pizza:
-         *       type: object
-         *       properties:
-         *         name:
-         *           type: string
-         *           description: Name of the pizza
-         *         description:
-         *           type: string
-         *           description: Description of the pizza
-         *         price:
-         *           type: number
-         *           description: Price of the pizza
-         *         type:
-         *           type: string
-         *           description: Type of pizza ("whole", "half")
-         *         thumbnail:
-         *           type: array
-         *           items:
-         *             type: string
-         *           default: [""]
-         *           description: Array of thumbnail URLs (["www.imagen1.com", "www.imagen2.com"])
-         */
-
-        /**
-         * @swagger
-         * tags:
-         *   - name: Pizzas
-         *     description: Operations about pizzas
-         */
-
-        /**
-         * @swagger
-         * /pizzas:
-         *   get:
-         *     summary: Get a list of available pizzas
-         *     tags:
-         *       - Pizzas
-         *     responses:
-         *       200:
-         *         description: List of pizzas retrieved successfully
-         *       500:
-         *         description: Internal server error
-         */
-
         //~> |Get a list of available pizzas
         this.get("/", pizzaController.getPizzas);
-
-        /**
-         * @swagger
-         * /pizzas/{pid}:
-         *   get:
-         *     summary: Retrieve a single pizza by ID
-         *     tags:
-         *       - Pizzas
-         *     parameters:
-         *       - name: pid
-         *         in: path
-         *         required: true
-         *         description: ID of the pizza to retrieve
-         *         schema:
-         *           type: string
-         *     responses:
-         *       200:
-         *         description: Pizza retrieved successfully
-         *       404:
-         *         description: Pizza not found
-         */
 
         // Retrieve a single Pizza with ID
         this.get("/:pid", pizzaController.getPizzaById);
 
-        /**
-         * @swagger
-         * /pizzas:
-         *   post:
-         *     summary: Add a new pizza
-         *     tags:
-         *       - Pizzas
-         *     requestBody:
-         *       required: true
-         *       content:
-         *         application/json:
-         *           schema:
-         *             $ref: '#/components/schemas/Pizza'
-         *     responses:
-         *       201:
-         *         description: Pizza added successfully
-         *       400:
-         *         description: Bad request
-         */
-
         //~> |Add Pizza
         this.post("/", pizzaController.addPizza);
-        /**
-         * @swagger
-         * /pizzas/{pid}:
-         *   put:
-         *     summary: Update a pizza by id
-         *     tags:
-         *       - Pizzas
-         *     parameters:
-         *       - name: pid
-         *         in: path
-         *         required: true
-         *         description: Name of the pizza to update
-         *         schema:
-         *           type: string
-         *     requestBody:
-         *       required: true
-         *       content:
-         *         application/json:
-         *           schema:
-         *             $ref: '#/components/schemas/Pizza'
-         *     responses:
-         *       204:
-         *         description: Pizza updated successfully
-         *       404:
-         *         description: Pizza not found
-         */
 
         //~> |Put Pizzas
         this.put("/:pid", pizzaController.updatePizzas);
-
-        /**
-         * @swagger
-         * /pizzas/{pid}:
-         *   delete:
-         *     summary: Delete a pizza by id
-         *     tags:
-         *       - Pizzas
-         *     parameters:
-         *       - name: pid
-         *         in: path
-         *         required: true
-         *         description: Name of the pizza to delete
-         *         schema:
-         *           type: string
-         *     responses:
-         *       204:
-         *         description: Pizza deleted successfully
-         *       404:
-         *         description: Pizza not found
-         */
 
         //~> |Delete Pizza by name
         this.delete("/:pid", pizzaController.deletePizzas);
