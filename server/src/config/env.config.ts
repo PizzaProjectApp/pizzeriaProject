c63d31455f527fb3cc46da8f61887854f2a08f67
Juudini <juudinidev@gmail.com>
2023-10-08 20:20:18 -0300

    feat(config/env): Added new dependencie to manage 'env' variables

--- server/src/config/env.config.ts
+++ server/src/config/env.config.ts
@@ -1,10 +1,7 @@
-import { config } from "dotenv";
+import "dotenv/config";
+import { get } from "env-var";
 
-config();
-
-// Set the server's port: use the value of PORT environment variable if defined, else default to 8080
-export const PORT = process.env.PORT || 3100;
-
-// Set the MongoDB URI: use the value of MONGODB_URI environment variable
-
-export const MONGODB_URI: string = process.env.MONGODB_URI || "";
+export const envs = {
+    PORT: get("PORT").required().asPortNumber(),
+    MONGODB_URI: get("MONGODB_URI").required().asString(),
+};
