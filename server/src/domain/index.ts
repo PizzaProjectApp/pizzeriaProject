ec7f9b80f17ff6aea1c8d13934096c3e04ce7970
Juudini <juudinidev@gmail.com>
2023-10-16 18:18:57 -0300

    feat(domain/index): Added new path

--- server/src/domain/index.ts
+++ server/src/domain/index.ts
@@ -1,5 +1,5 @@
 export * from "./entities/pizza.entity";
-export * from "./dtos/pizza.dto";
+export * from "./dtos/pizza/pizza.dto";
 export * from "./datasources/pizza.datasource";
 export * from "./repositories/pizza.repository";
 export * from "./errors/custom.error";
