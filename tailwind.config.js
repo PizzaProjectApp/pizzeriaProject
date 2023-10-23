a2237521c861bdefc8c8b4c46fa19cd6c58685a8
Juudini <juudinidev@gmail.com>
2023-10-23 00:52:48 -0300

    feat: Added custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        main: {
+          marron: "#412512",
+          quesito: "#df9a39",
+          chanchito: "#ecd0b8",
+          green: "#28a745",
+          teal: "#20c997",
+          cyan: "#17a2b8",
+          ligth: "#f1efdd",
+          tomate: "#e54424"
+        }
+      }
+    }
+  },
+  plugins: []
 };
 
