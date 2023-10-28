e47d0aa0cb89fae458987618251e0e488698dc14
Juudini <juudinidev@gmail.com>
2023-10-28 14:44:21 -0300

    fix(shared/pagination): Fixed totalPages result and added docs

--- server/src/domain/shared/pagination.ts
+++ server/src/domain/shared/pagination.ts
@@ -5,22 +5,15 @@ interface ExecutePaginationProps {
     page: number;
     sort: Sort;
     productUrl: string;
-    totalPages: number;
+    docs: number;
     products: any;
 }
 
-export const executePagination = async ({
-    page,
-    limit,
-    sort,
-    productUrl,
-    totalPages,
-    products
-}: ExecutePaginationProps) => {
+export const executePagination = async ({ page, limit, sort, productUrl, docs, products }: ExecutePaginationProps) => {
     const baseUrl: string = `/api/v1/${productUrl}?limit=${limit}&sort=${sort}`;
 
     let hasPrevPage: boolean = page > 1;
-    let hasNextPage: boolean = page < totalPages;
+    let hasNextPage: boolean = page < docs;
 
     let prevPage: number | null = hasPrevPage ? page - 1 : null;
     let nextPage: number | null = hasNextPage ? page + 1 : null;
@@ -28,10 +21,13 @@ export const executePagination = async ({
     let prevLink: string | null = hasPrevPage ? `${baseUrl}&page=${prevPage}` : null;
     let nextLink: string | null = hasNextPage ? `${baseUrl}&page=${nextPage}` : null;
 
+    const totalPages: number = Math.ceil(docs / limit);
+
     const results = {
         status,
         payload: products,
         page,
+        docs,
         totalPages,
         hasPrevPage,
         hasNextPage,
